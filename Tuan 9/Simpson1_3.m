fc9994b1744a64a5f0e292fa5430582cb7ea5b9b
FoveIT <tranvantu7597@gmail.com>
2021-05-25 10:32:20 +0700

    update

--- Tuan 9/Simpson1_3.m
+++ Tuan 9/Simpson1_3.m
@@ -1,19 +1,23 @@
 function [S] = Simpson1_3(xx,yy,df)
     n = length(xx)
     S = 0 
-    loop = round(n/3,0) 
+    loop = round(n/2-1,0) 
+    
+%     1/3
     for i = 0:loop-1
         id = 2*i+1
         S = S + 1/6*(xx(id+2)- xx(id))*(yy(id+2) + 4*yy(id+1)+yy(id)) 
     end
-    if(loop*3 ==n)
-        return;
+    
+    % hinh thang
+    sodu = n - round(n/3,0)*3
+    
+    if(sodu==1)
+        S = S + xx(n)*yy(n)
     end
-    if(n-loop*3 == 1)
-        n
-        S = S + xx(end)
+    if(sodu==-1)
+        S  = S + (xx(n)-xx(n-1))/2*(yy(n-1)+yy(n))
     end
-     if(n-loop*3 == 2)
-        S = S +  0.5*(xx(n)- xx(n-1))*(yy(n-1)+yy(n)) ;
-    end
+    
+    
 end
